0729cee93222cf94e1f7fb338c25d37365fa2d87
msaviola <67287151+msaviola@users.noreply.github.com>
2020-08-12 11:01:06 -0700

    ajax attempts

--- script.js
+++ script.js
@@ -2,72 +2,66 @@ var cityInput = document.querySelector("#city-text")
 var cityForm = document.querySelector("#locations-form")
 var cityList = document.querySelector("#city-list")
 var cities = ["New York", "Austin", "Chicago"];
-var CityCount ="";
+var CityCount = "";
 
 renderCities();
 
-function queryURL(){
-var queryURL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=";
-var key= { "api-key": "34c625f0fb47ccd59d343b8dead43393"};
-queryParams.q = $("#city-text")
-    .val()
-    .trim();
+var queryURL = "api.openweathermap.org/data/2.5/forecast?q={Chicago}&appid={34c625f0fb47ccd59d343b8dead43393}"
+var key = "&appid={34c625f0fb47ccd59d343b8dead43393}"
 
-  // Logging the URL so we have access to it for troubleshooting
-  console.log("---------------\nURL: " + queryURL + "\n---------------");
-  console.log(queryURL + $.param(queryParams));
-  return queryURL + $.param(queryParams);
+//var cityNameInput = $("#city-text")
+var city="q={chichago}"
+
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function (response) {
+  console.log(queryURL);
+  console.log(url);
+  console.log(response);
+})
+
+// Logging the URL so we have access to it for troubleshooting
+
+
+
+
+
+
+
+
+
+function renderCities() {
+  cityList.innerHTML = "";
+  CityCount.textContent = cities.length;
+
+  for (var i = 0; i < cities.length; i++) {
+    var city = cities[i];
+
+    var li = document.createElement("button");
+    li.textContent = city;
+    li.classList.add("btn", "btn-secondary");
+
+    cityList.appendChild(li);
+  }
+
 }
 
-$("#run-search").on("click", function(event) {
-    // This line allows us to take advantage of the HTML "submit" property
-    // This way we can hit enter on the keyboard and it registers the search
-    // (in addition to clicks). Prevents the page from reloading on form submit.
-    event.preventDefault();
-  
-    // Empty the region associated with the articles
-    clear();
-  
-    // Build the query URL for the ajax request to the NYT API
-    var queryURL = buildQueryURL();
-  
-    // Make the AJAX request to the API - GETs the JSON data at the queryURL.
-    // The data then gets passed as an argument to the updatePage function
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(updatePage);
-  });
+cityForm.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-function renderCities(){
-    cityList.innerHTML = "";
-    CityCount.textContent = cities.length;
+  var cityText = cityInput.value.trim();
 
-    for(var i=0; i<cities.length; i++){
-        var city = cities[i];
+  if (cityText === "") {
+    return;
+  }
 
-        var li = document.createElement("button");
-        li.textContent = city;        
-        li.classList.add("btn","btn-secondary");
-        
-        cityList.appendChild(li);
-    }
-    
-}
+  cities.push(cityText);
+  cityInput.value = "";
 
-cityForm.addEventListener("submit", function(event){
-    event.preventDefault();
-
-    var cityText = cityInput.value.trim();
-
-    if (cityText ===""){
-        return;
-    }
-
-    cities.push(cityText);
-    cityInput.value = "";
-
-    renderCities();
+  renderCities();
 
 
 });
+
+
